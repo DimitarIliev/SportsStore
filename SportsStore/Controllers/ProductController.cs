c1036a38c71a912a47d0c248c7a9098f9ea09b8f
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-02-17 19:29:27 +0100

    Initial commint.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _reposotiry;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repository)
+        {
+            _reposotiry = repository;
+        }
+
+        public ViewResult List(string category, int page = 1) => View(new ProductsListViewModel
+        {
+            Products = _reposotiry.Products
+            .Where(x => x.Category == category || category == null)
+            .OrderBy(x => x.ProductId)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = _reposotiry.Products.Count()
+            },
+            CurrentCategory = category
+        });
+    }
+}
---
+++ SportsStore/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(environment.ContentRootPath).AddJsonFile("appsettings.json").Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}/Page{page:int}",
+                    defaults: new { controller = "Product", action = "List" });
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{page:int}",
+                    defaults: new { controller = "Product", action = "List", page = 1 });
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "{category}",
+                    defaults: new { Controller = "Product", action = "List", page = 1});
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", page = 1 });
+
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+            });
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
