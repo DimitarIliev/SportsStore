8c71b76978fbf3312cd20d325dac7883be3693f6
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-02-24 20:25:44 +0100

    Completed Cart, Order & Checkout functionalities.

---
+++ SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext _context;
+        public IEnumerable<Order> Orders => _context.Orders.Include(x => x.Lines).ThenInclude(x => x.Product);
+
+        public EFOrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveOrder(Order order)
+        {
+            _context.AttachRange(order.Lines.Select(x => x.Product));
+
+            if (order.OrderId == 0)
+                _context.Orders.Add(order);
+
+            _context.SaveChanges();
+        }
+    }
+}
